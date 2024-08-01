13824be9f5b39e665e15e1c72c145e86a52b50ee
User Name <zbrustkern@gmail.com>
2024-08-01 09:57:17 -0700

    rebuild and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello, firebase world!</h1>
+      <h1>Hello, firebase world! Now Hosted?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
