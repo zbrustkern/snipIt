9342729973cf15ab8c2c8dfdc39fd6e531b4cc62
User Name <zbrustkern@gmail.com>
2024-08-01 11:37:21 -0700

    tailwind installed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello, firebase world! Now Hosted?</h1>
+      <h1 className="text-3xl font-bold underline">Hello, firebase world! Now Hosted?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
